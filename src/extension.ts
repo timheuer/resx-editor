48a53cb14e47cfcc56d88b378593de122b3d81c4
Tim Heuer <tim@timheuer.com>
2023-06-23 18:52:17 -0700

    Removing registration of command

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,9 @@ import { ResxProvider } from './resxProvider';
 
 
 export function activate(context: vscode.ExtensionContext) {
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('resx-viewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		//HelloWorldPanel.render(context.extensionUri);
-	});
-
+	
 	context.subscriptions.push(ResxProvider.register(context));
-	context.subscriptions.push(disposable);
+	
 }
 
 export function deactivate() {}
